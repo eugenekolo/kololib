5c6297a10fffd64d08a617739186868dcc67a983
Eugene Kolo <eugene@kolobyte.com>
2016-08-01 01:43:20 -0400

    Cleaning bit up

--- c/typedefs.h
+++ c/typedefs.h
@@ -1,5 +1,7 @@
 #include <sys/types.h>
+
 #if defined(_WIN32) && !defined(__MINGW32__) && (!defined(_MSC_VER) || _MSC_VER<1600)
+
 #include <BaseTsd.h>
 #include <stddef.h>
 typedef __int8 int8_t;
@@ -10,6 +12,9 @@ typedef __int32 int32_t;
 typedef unsigned __int32 uint32_t;
 typedef __int64 int64_t;
 typedef unsigned __int64 uint64_t;
+
 #else
+
 #include <stdint.h>
+
 #endif
